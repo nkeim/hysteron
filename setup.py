f250a045cf1ac35042623e202015f12f8fd95596
Nathan Keim <keim@psu.edu>
2021-08-26 12:43:37 -0400

    Remove redundant numpy requirement

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open('README.rst') as readme_file:
 with open('HISTORY.rst') as history_file:
     history = history_file.read()
 
-requirements = ['numba>=0.51', 'numpy']
+requirements = ['numba>=0.51']
 
 setup_requirements = [ ]
 
